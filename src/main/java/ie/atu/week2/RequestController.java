a231e70f283aa1f74578e658be20ac266d5ed39f
Rory O Loughlin <g00422319@atu.ie>
2024-12-28 11:43:31 +0000

    exercise3 done

--- src/main/java/ie/atu/week2/RequestController.java
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -2,6 +2,7 @@ package ie.atu.week2;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +13,13 @@ public class RequestController {
         return "Hello, world!";
     }
 
-
     @GetMapping("/greet/{name}")
     public String greetByName(@PathVariable String name) {
         return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/details")
+    public String details(@RequestParam String name, @RequestParam int age) {
+        return "Name: " + name + ", Age: " + age;
     }
 }
